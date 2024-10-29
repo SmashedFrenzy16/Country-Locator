3c2f8ef8e8e0bd212e7ab0007488a1423f847167
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-10-29 13:37:42 +0000

    Update country_locator.py with title labl

--- country_locator.py
+++ country_locator.py
@@ -5,4 +5,10 @@ root = Tk()
 
 root.title("Country Locator")
 
+label = Label(root, text="Country Locator", font=("Arial", 24))
+label.pack()
+
+blank_label = Label(root, text="")
+blank_label.pack()
+
 root.mainloop()
