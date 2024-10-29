49b6faa62b55a3bc0378960d0b2645338759ecd1
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-10-29 13:56:27 +0000

    Update country_locator.py with entry box for input

--- country_locator.py
+++ country_locator.py
@@ -11,4 +11,9 @@ label.pack()
 blank_label = Label(root, text="")
 blank_label.pack()
 
+place_entry = Entry(root, width=100, borderwidth=5)
+place_entry.pack()
+
+place_entry.insert(0, "Enter in the name of the place you want to locate")
+
 root.mainloop()
